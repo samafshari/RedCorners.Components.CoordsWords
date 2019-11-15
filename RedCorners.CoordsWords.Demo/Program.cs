0700fd2aa68884c2cacd3015133b328763e73716
s_a@live.com (NEATBOOK2) <s_a@live.com>
2019-11-15 22:46:33 +0100

    CoordsWords base

---
+++ RedCorners.CoordsWords.Demo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using RedCorners;
+
+namespace RedCorners.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coordsWords = new CoordsWords()
+        }
+    }
+}
